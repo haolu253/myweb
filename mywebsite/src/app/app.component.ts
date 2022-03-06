b73addb8b8be305a42609f26cc07d8fc75655ba7
haolu253 <luchihao253@gmail.com>
2022-03-06 18:32:25 +0700

    update samsung bug

--- mywebsite/src/app/app.component.ts
+++ mywebsite/src/app/app.component.ts
@@ -12,5 +12,12 @@ export class AppComponent {
 
   ngOnInit(){
     AOS.init();
+    if (navigator.userAgent.match(/samsung/i)) {
+      alert("You are using a defective browser (Samsung Internet) that " +
+      "might not be configured to display this website properly. " +
+      "You should consider using a proper standards-compliant " +
+      "browser instead. \n\n"+
+      "We recommend using Firefox, Microsoft Edge, or Google Chrome.");   
+    }   
   }
 }
